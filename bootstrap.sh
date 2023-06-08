981d3fee1c92826d6bb067be86d435a16b27a06b
Guang Yang <guangyang@ticktack.no>
2023-06-08 15:56:04 +0200

    asdfaf

---
+++ bootstrap.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+set -x  # Enable bash debug
+exec &> /var/log/boot.log
+yum update -y
+yum install -y docker git expect
+systemctl enable docker
+#systemctl enable named
+systemctl start docker
+cd /root
+# Fetch the IP address
+while true; do
+  IP_ADDRESS=$(dig +short myip.opendns.com @resolver1.opendns.com)
+  if [ $? -ne 0 ]; then
+    echo "Command failed, retrying..."
+    sleep 10  # Add a delay if desired
+  else
+    break
+  fi
+done
+# Clone the repository
+git clone https://github.com/kylemanna/docker-openvpn.git
+# Change current directory to the cloned repository
+cd docker-openvpn
+
+# Build the Docker image
+docker build -t myownvpn .
+# Change back to the previous directory and create a volume for storing config files and keys
+cd ..
+mkdir vpn-data-443 && touch vpn-data-443/vars
+
+# Generate the OpenVPN config file
+docker run -v $PWD/vpn-data-443:/etc/openvpn --rm myownvpn ovpn_genconfig -u udp://${IP_ADDRESS}:443
+PASSPHRASE=$(cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 8; echo;)
+# Initialize PKI, generate the CA certificate
+/usr/bin/expect <<EOF
+set timeout 600
+
+spawn docker run -v $PWD/vpn-data-443:/etc/openvpn --rm -it myownvpn ovpn_initpki
+expect "Enter New CA Key Passphrase:" { send "$PASSPHRASE\r" }
+expect "Confirm New CA Key Passphrase:" { send "$PASSPHRASE\r" }
+expect "Common Name (eg: your user, host, or server name)" { send "$IPADDRESS\r" }
+expect "Confirm request details" { send "yes\r" }
+expect -re "Enter pass phrase for /etc/openvpn/pki/private/ca.key:" { send "$PASSPHRASE\r" }
+expect -re "Enter pass phrase for /etc/openvpn/pki/private/ca.key:" { send "$PASSPHRASE\r" }
+expect "CRL file: /etc/openvpn/pki/crl.pem" { send "\r" }
+EOF
+
+
+
+
+# Run the VPN server
+docker run -v $PWD/vpn-data-443:/etc/openvpn -d -p 443:1194/udp --cap-add=NET_ADMIN myownvpn
+
+# Create a user for the VPN connection
+for i in xp zt xw; do
+  /usr/bin/expect <<EOF
+  set timeout 600
+
+  spawn docker run -v $PWD/vpn-data-443:/etc/openvpn --rm -it myownvpn easyrsa build-client-full $i nopass
+  expect "Confirm request details" { send "yes\r" }
+  expect -re "Enter pass phrase for /etc/openvpn/pki/private/ca.key:" { send "$PASSPHRASE\r" }
+  expect "Signature ok" { send "\r" }
+EOF
+done
+
+# Generate a configuration file for the user
+for i in xp zt xw; do
+docker run -v $PWD/vpn-data-443:/etc/openvpn --rm myownvpn ovpn_getclient $i > $i.ovpn
+sed -i 's/redirect-gateway def1/route 192.168.0.0\/24 255.255.0.0/g' $i.ovpn
+echo "redirect-gateway def1" >> $i.ovpn
+mv $i.ovpn /home/ec2-user/$i.ovpn
+chown ec2-user /home/ec2-user/$i.ovpn
+done
+
+# Enable IP forwarding
+echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
+sysctl -p
+
+# Update iptables
+iptables -I FORWARD -j ACCEPT
+iptables -t nat -I POSTROUTING -s 192.168.255.0/24 -o eth0 -j MASQUERADE
+
+# Setup bind service
+#cat <<EOF> /etc/named.conf
+#acl "allowed" { ${IP_ADDRESS}/32; };
+#options {
+#    forwarders {
+#        8.8.8.8;   // Specify your upstream DNS server IP address here
+#        8.8.4.4;
+#    };
+#    forward only;
+#    allow-query { "allowed"; };
+#};
+#EOF
+#systemctl start named
---
+++ vpn.tf
@@ -0,0 +1,109 @@
+provider "aws" {
+  region = "ap-east-1"
+}
+
+locals {
+  project_name = "openvpn"
+}
+
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+}
+
+module "vpc" {
+  source                  = "terraform-aws-modules/vpc/aws"
+  map_public_ip_on_launch = true
+  cidr                    = "10.0.0.0/16"
+  azs                     = ["ap-east-1a", "ap-east-1b"]
+  private_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets          = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  #enable_nat_gateway = true
+  #enable_vpn_gateway = false
+
+  tags = {
+    Name = local.project_name
+  }
+
+}
+
+#security group
+resource "aws_security_group" "web-sg" {
+  vpc_id = module.vpc.vpc_id
+  name   = "web_sg"
+
+  ingress {
+    from_port   = 0
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    protocol    = "udp"
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 53
+    protocol    = "tcp"
+    to_port     = 53
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 53
+    protocol    = "udp"
+    to_port     = 53
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = local.project_name
+  }
+}
+
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t3.micro"
+
+  key_name = aws_key_pair.this.key_name
+
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = module.vpc.public_subnets[0]
+  user_data = file("${path.module}/bootstrap.sh")
+
+  associate_public_ip_address = true
+  tags = {
+    Name = local.project_name
+  }
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = local.project_name
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+output "aws_instance_public_ip" {
+  value = aws_instance.this.public_ip
+}
